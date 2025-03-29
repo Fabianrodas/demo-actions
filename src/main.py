5e61be9099ccb2c2184462c2e54a1422722f5a15
Fabián Rodas <fabianrodas2005@gmail.com>
2025-03-29 11:41:56 -0500

    Add files

---
+++ src/main.py
@@ -0,0 +1,9 @@
+# src/main.py
+import os
+
+# obtener entrada
+valor = os.environ.get("INPUT_VAL")
+
+if "GITHUB_OUTPUT" in os.environ : 
+    with open(os.environ["GITHUB_OUTPUT"], "a") as f :
+        print("{0}= Hola {1}".format('result', valor), file=f)
